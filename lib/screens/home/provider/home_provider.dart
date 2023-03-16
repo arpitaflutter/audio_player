88ce7e80a96bc3c69683634b9ca1816ff56b03df
arpitaflutter <arpitamangukiya2000@gmail.com>
2023-03-16 11:34:00 +0530

    commit

--- lib/screens/home/provider/home_provider.dart
+++ lib/screens/home/provider/home_provider.dart
@@ -4,13 +4,23 @@ import 'package:flutter/cupertino.dart';
 class home_provider extends ChangeNotifier
 {
   AssetsAudioPlayer? assetsAudioPlayer;
+  bool isMute = false;
+  Duration totalDuration = Duration(seconds: 0);
+
+  List<Audio> AudioList = [
+    Audio("assets/music/omw.mp3"),
+    Audio("assets/music/Jeena Jeena.mp3"),
+    Audio("assets/music/Jeene Laga Hoon.mp3"),
+  ];
   
   void initAudio()
   {
     assetsAudioPlayer = AssetsAudioPlayer();
-    assetsAudioPlayer!.open(Audio("assets/music/omw.mp3"),
+    assetsAudioPlayer!.open(Playlist(audios: AudioList),
       autoStart: false,showNotification: true
     );
+
+    totalDurationAudio();
   }
   
   Future<void> startAudio()
@@ -23,7 +33,29 @@ class home_provider extends ChangeNotifier
     await assetsAudioPlayer!.pause();
   }
   
-  void muteAudio() {
-    
+  Future<void> muteAudio() async {
+    await assetsAudioPlayer!.setVolume(isMute?1:0);
+    isMute = !isMute;
+    notifyListeners();
+  }
+
+  void totalDurationAudio()
+  {
+    assetsAudioPlayer!.current.listen((event) {
+      totalDuration = event!.audio.duration;
+      notifyListeners();
+    });
+  }
+
+  Future<void> previousSong()
+  async {
+    await assetsAudioPlayer!.previous();
+    notifyListeners();
+  }
+
+  Future<void> nextSong()
+  async {
+    await assetsAudioPlayer!.next();
+    notifyListeners();
   }
 }
--- lib/screens/home/view/home_screen.dart
+++ lib/screens/home/view/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:audio_player/screens/home/provider/home_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -10,7 +11,6 @@ class home_screen extends StatefulWidget {
 }
 
 class _home_screenState extends State<home_screen> {
-
   home_provider? homepProvider_false;
   home_provider? homepProvider_true;
 
@@ -18,14 +18,13 @@ class _home_screenState extends State<home_screen> {
   void initState() {
     super.initState();
 
-    Provider.of<home_provider>(context,listen: false).initAudio();
+    Provider.of<home_provider>(context, listen: false).initAudio();
   }
 
   @override
   Widget build(BuildContext context) {
-
-    homepProvider_false = Provider.of<home_provider>(context,listen: false);
-    homepProvider_true = Provider.of<home_provider>(context,listen: true);
+    homepProvider_false = Provider.of<home_provider>(context, listen: false);
+    homepProvider_true = Provider.of<home_provider>(context, listen: true);
 
     return SafeArea(
       child: Scaffold(
@@ -38,34 +37,103 @@ class _home_screenState extends State<home_screen> {
               fit: BoxFit.cover,
             ),
             Container(
-              height: double.infinity,width: double.infinity,color: Colors.black45,
+              height: double.infinity,
+              width: double.infinity,
+              color: Colors.black45,
             ),
             Align(
               alignment: Alignment.center,
               child: ClipRRect(
-                child: Image.asset("assets/music/photo.webp",width: 200,height: 200,fit: BoxFit.cover),
+                child: Image.asset("assets/music/photo.webp",
+                    width: 200, height: 200, fit: BoxFit.cover),
                 borderRadius: BorderRadius.circular(20),
               ),
             ),
             Align(
               alignment: Alignment.bottomCenter,
               child: Padding(
-                padding: const EdgeInsets.only(bottom: 70),
+                padding: const EdgeInsets.only(bottom: 40),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    IconButton(onPressed: () {
-                      homepProvider_false!.startAudio();
-                    }, icon: Icon(Icons.play_circle,color: Colors.white,size: 35,)),
-                    IconButton(onPressed: () {
-                      homepProvider_false!.stopAudio();
-                    }, icon: Icon(Icons.pause_circle,color: Colors.white,size: 35,)),
-                    IconButton(onPressed: () {
-
-                    }, icon: Icon(Icons.volume_mute,color: Colors.white,size: 35,))
+                    IconButton(
+                        onPressed: () {
+                          homepProvider_false!.startAudio();
+                        },
+                        icon: Icon(
+                          Icons.play_circle,
+                          color: Colors.white,
+                          size: 35,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          homepProvider_false!.stopAudio();
+                        },
+                        icon: Icon(
+                          Icons.pause_circle,
+                          color: Colors.white,
+                          size: 35,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          homepProvider_true!.muteAudio();
+                        },
+                        icon: Icon(
+                          homepProvider_false!.isMute
+                              ? Icons.volume_mute
+                              : Icons.keyboard_voice,
+                          color: Colors.white,
+                          size: 35,
+                        ))
                   ],
                 ),
               ),
+            ),
+            PlayerBuilder.currentPosition(
+              player: homepProvider_false!.assetsAudioPlayer!,
+              builder: (context, position) {
+                return Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Slider(
+                        value: position.inSeconds.toDouble(),
+                        onChanged: (value) {
+                          homepProvider_false!.assetsAudioPlayer!
+                              .seek(Duration(seconds: value.toInt()));
+                        },
+                        activeColor: Colors.white,
+                        inactiveColor: Colors.white54,
+                        max: homepProvider_true!.totalDuration.inSeconds
+                            .toDouble(),
+                      ),
+                      SizedBox(height: 10,),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 50,left: 10,right: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("$position",style: TextStyle(color: Colors.white)),
+                            Text("${homepProvider_false!.totalDuration}",style: TextStyle(color: Colors.white),)
+                          ],
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(onPressed: () {
+                            homepProvider_true!.previousSong();
+                          }, icon: Icon(Icons.skip_previous,color: Colors.white,)),
+                          IconButton(onPressed: () {
+                            homepProvider_true!.nextSong();
+                          }, icon: Icon(Icons.skip_next,color: Colors.white,)),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
           ],
         ),
